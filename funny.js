548c11b73165392ece3fd15eba33675ce73078c1
Anthony Huang <90001131+anthonyhuang07@users.noreply.github.com>
2022-05-11 21:15:02 -0400

    Add files via upload

--- funny.js
+++ funny.js
@@ -1,3 +1,5 @@
+let current = new Date();
+document.getElementById("title").innerHTML = `Hello! Today is ${current.toLocaleDateString()}.`;
 document.querySelector('#blob').addEventListener('click', function() {
     alert('The blob is minding his own business. Leave him alone.');
 });
