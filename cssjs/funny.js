c8d2ab134312028fc84aed30d7ae59d2b661939b
Anthony Huang <90001131+anthonyhuang07@users.noreply.github.com>
2022-05-30 09:22:38 -0400

    Delete funny.js

--- cssjs/funny.js
+++
@@ -1,5 +0,0 @@
-let current = new Date();
-document.getElementById("title").innerHTML = `Hello! Today is ${current.toLocaleDateString()}.`;
-document.querySelector('#blob').addEventListener('click', function() {
-    alert('The blob is minding his own business. Leave him alone.');
-});
