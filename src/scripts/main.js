1580a38e39137910eb07303b87f063587b18bf7b
Anthony Huang <anthonyhuang07@icloud.com>
2023-01-09 08:09:14 -0500

    changing the copyright date:sunglasses:

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,19 @@
-let current = new Date();
-document.getElementById("title").innerHTML = `Hello! Today is <span class="date">${current.toLocaleDateString()}.</span>`;
+const date1 = document.getElementById("date1")
+const date2 = document.getElementById("date2")
+const header = document.getElementById("h-hide")
+let yPos = window.scrollY
+function split(){
+    date1.style.transform = "rotate(-"+(window.scrollY/20)+"deg) translate(-"+(window.scrollY/2)+"px)";
+    date2.style.transform = "rotate("+(window.scrollY/20)+"deg) translate("+(window.scrollY/2)+"px)";
+    date1.style.opacity = ("1")-(window.scrollY/1000);
+    date2.style.opacity = ("1")-(window.scrollY/1000);  
+    console.log((1)-(window.scrollY/1000))
+}
+function headerHide(){
+    if (yPos < window.scrollY){
+        header.style.top = "-8vh";
+    } else if (yPos > window.scrollY){
+        header.style.top = "0";
+    }
+    yPos = window.scrollY
+}
