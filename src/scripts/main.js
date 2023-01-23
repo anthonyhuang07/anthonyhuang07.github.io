59cc9ccda75b0bfb453ab80a3f4fa9e1fa56c389
Anthony Huang <anthonyhuang07@icloud.com>
2023-01-23 13:33:52 -0500

    please work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,3 +17,7 @@ function headerHide(){
     }
     yPos = window.scrollY
 }
+
+
+let current = new Date();
+document.getElementById("title").innerHTML = `Hello! Today is <span class="date">${current.toLocaleDateString()}.</span>`;
