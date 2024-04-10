d8823549436b4900e8b813415d79f10c87ee3b8b
Anthony Huang <anthonyhuang07@icloud.com>
2024-04-10 12:14:17 -0400

    basic "currently listening to"

---
+++ src/scripts/music.js
@@ -0,0 +1,20 @@
+const nowplaying = document.getElementById('nowplaying');
+const apiUrl = 'http://ah07.ddns.net:4000/api/status';
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    if(data.status[0].name === "Apple Music"){
+        nowplaying.innerHTML = `Currently Listening To: ${data.status[0].details} by ${data.status[0].state}.`
+    } else {
+        nowplaying.innerHTML = `Currently Listening To: Nothing.`
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
