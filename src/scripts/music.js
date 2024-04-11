2f6e3d68119d9d4534f7a99adf4f2ba40f33bcdf
Anthony Huang <anthonyhuang07@icloud.com>
2024-04-11 01:33:25 -0400

    CONSTANT UPDATING

--- src/scripts/music.js
+++ src/scripts/music.js
@@ -1,23 +1,30 @@
 const nowplaying = document.getElementById('nowplaying');
 const apiUrl = 'https://server.ah07.xyz/api/status';
 
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    for (let i = 0; i < data.status.length; i++) {
-      if (data.status[i].name === "Apple Music") {
-        nowplaying.innerHTML = `Currently Listening To: ${data.status[i].details} by ${data.status[i].state}.`
-        break
-      } else {
-        nowplaying.innerHTML = `Currently Listening To: Nothing.`
+function nowPlaying() {
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
-    }
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+      return response.json();
+    })
+    .then(data => {
+      for (let i = 0; i < data.status.length; i++) {
+        if (data.status[i].name === "Apple Music") {
+          nowplaying.innerHTML = `Currently Listening To: ${data.status[i].details} by ${data.status[i].state}.`
+          break
+        } else {
+          nowplaying.innerHTML = `Currently Listening To: Nothing.`
+        }
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+
+}
+
+nowPlaying()
+
+setInterval(nowPlaying, 1000);
