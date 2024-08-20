9a38f3455855c35cdb7bee1df8f4d222f72f730e
Anthony Huang <anthonyhuang07@icloud.com>
2024-08-20 00:19:56 -0400

    Added About Me page - mobile optimization efforts continue tomorrow

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,11 @@
-const date = document.getElementById("date")
-const lang = document.getElementById("randLang")
+const inapp = document.querySelector("#inApp")
+const appStore = document.querySelector(".appStore")
 
-// set date in footer
-d = new Date()
-date.innerHTML = "Copyright (c) " + d.getFullYear() + " Anthony Huang"
-
-const langs = ["Hello", "Bonjour", "你好"]
-
-lang.innerHTML = langs[Math.floor(Math.random() * langs.length)]
+function appOpen(app){
+    inapp.style.display = "block";
+    switch(app){
+        case 1:
+            appStore.style.display = "block";
+            break;
+    }
+}
