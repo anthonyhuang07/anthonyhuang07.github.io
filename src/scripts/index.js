74199f438832e1486c988f8ff8e00549e0ee01a9
Anthony Huang <anthonyhuang07@icloud.com>
2024-08-20 15:40:19 -0400

    reduce motion transition

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,12 @@ const inapp = document.querySelector("#inApp")
 const appStore = document.querySelector(".appStore")
 
 function appOpen(app){
-    inapp.style.display = "block";
+    inapp.style.visibility = "visible";
+    inapp.style.opacity = "1";
     switch(app){
         case 1:
-            appStore.style.display = "block";
+            appStore.style.visibility = "visible";
+            appStore.style.opacity = "1";
             break;
     }
 }
