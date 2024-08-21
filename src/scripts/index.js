a11afdf3dc8c8d3af7d7e9c90d4c538ddab7a499
Anthony Huang <anthonyhuang07@icloud.com>
2024-08-20 22:30:06 -0400

    everything is good... except for mobile. View buttons coming soon

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,9 @@
 const inapp = document.querySelector("#inApp")
 const appStore = document.querySelector(".appStore")
 
+const rankWidth = document.querySelector(".rank").offsetWidth;
+document.documentElement.style.setProperty('--rank-width', `${rankWidth}px`);
+
 function appOpen(app){
     inapp.style.visibility = "visible";
     inapp.style.opacity = "1";
@@ -10,4 +13,11 @@ function appOpen(app){
             appStore.style.opacity = "1";
             break;
     }
+}
+
+function returnHome(){
+    inapp.style.visibility = "hidden";
+    appStore.style.visibility = "hidden";
+    inapp.style.opacity = "0";
+    appStore.style.opacity = "0";
 }
