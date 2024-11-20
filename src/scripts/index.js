1e960683c90d93ec87eddfb8bff215000b43e8dd
Anthony Huang <anthonyhuang07@icloud.com>
2024-11-20 17:15:57 -0500

    :

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,18 @@
 const inApp = document.querySelector("#inApp")
 const menu = document.querySelector(".menu")
 const appStore = document.querySelector(".appStore")
+const projects = document.querySelector(".projects")
 const contacts = document.querySelector(".contacts")
+const appButton = document.querySelector(".appButton")
+const appView = document.querySelector(".appView")
 
 window.onkeydown = (e) => {
     if(e.keyCode == 27){
-        returnHome()
+        if (appView.style.visibility === "visible") {
+            hideAppView();
+        } else {
+            returnHome();
+        }
     }
 }
 
@@ -20,6 +27,9 @@ function appOpen(app){
             appStore.style.opacity = "1";
             break;
         case 2:
+            projects.style.visibility = "visible";
+            projects.style.overflow = "auto";
+            projects.style.opacity = "1";
             break;
         case 3:
             contacts.style.visibility = "visible";
@@ -30,7 +40,19 @@ function appOpen(app){
 
 function returnHome(){
     document.body.style.overflow = "hidden";
-    inApp.style.overflow = menu.style.overflow = "hidden"
-    inApp.style.visibility = appStore.style.visibility = contacts.style.visibility = "hidden";
-    inApp.style.opacity = menu.style.opacity = "0";
+    inApp.style.overflow = menu.style.overflow = appView.style.overflow = "hidden";
+    inApp.style.visibility = appStore.style.visibility = appView.style.visibility = projects.style.visibility = contacts.style.visibility = "hidden";
+    inApp.style.opacity = menu.style.opacity = appView.style.opacity = "0";
 }
+
+function hideAppView(){
+    appView.style.visibility = "hidden"
+    appView.style.opacity = "0"
+    appView.style.overflow = "hidden"
+}
+
+appButton.addEventListener("click", () => {
+    appView.style.visibility = "visible"
+    appView.style.opacity = "1"
+    appView.style.overflow = "auto"
+})
