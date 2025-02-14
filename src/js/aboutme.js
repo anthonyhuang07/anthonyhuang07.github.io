fb8bba0cb70fbf154237fec3ed401e4834a1fc4e
Anthony Huang <anthonyhuang07@icloud.com>
2025-02-14 01:53:35 -0500

    update appDetails in aboutme.js with improved descriptions and links; adjust font size in aboutme.css

--- src/js/aboutme.js
+++ src/js/aboutme.js
@@ -7,63 +7,82 @@ const appDetails = {
         I'm a 17-year-old high school student from Ontario, Canada, born in Québec and of Chinese descent. 
         
         I have a strong passion for all things tech and computing, as well as linguistics and geography!`,
-        openLink: "#",
+        link: "https://www.linkedin.com/in/anthonyhuang07/",
         icon: "https://cdn.iconscout.com/icon/free/png-256/free-find-my-icon-download-in-svg-png-gif-file-formats--iphone-ios-13-ios13-ipad-os-14-pack-user-interface-icons-1575949.png?f=webp&w=256"
     },
     "2": {
         title: "Programming",
-        subtitle: "HTML/CSS, JS/TS, Python",
-        description: "I started programming at 8 years old with p5.js on Khan Academy, and later spent a lot of time Scratch. When I was 10, I learned basic web development with HTML/CSS\n\nI made my return to programming when I was 13, after my computer history/hardware/YouTuber phase. I learned basic C to get back into it, and then I finally formally learned JavaScript, where I made my first side project, a Discord Bot. Then I started making websites again, where it is still my primary specialty to this day. In later years, I picked up Python and TypeScript.",
-        openLink: "#",
+        subtitle: "HTML/CSS, JS/TS, Python.",
+        description: `Almost everything I know in this field is self-taught. I started programming at 8 years old with p5.js on Khan Academy, and later spent a lot of time Scratch. When I was 10, I learned basic web development with HTML/CSS.
+        
+        I made my return to programming at 13, after learning much about the tech world outside of just coding. I learned basic C to get back the vibe, then JavaScript, where I built my first side project—a Discord bot. Web development soon became my primary focus, and I later learned Python and TypeScript.
+        
+        I've participated in 5 hackathons, earning awards in 2 of them. I sometimes dabble in competitive programming, I primarily use Python and took part in the CCC Junior, where I scored 56/75.
+        
+        I am a soon-to-be Computer Science student in university. I hope to explore more about the fields of full stack and cybersecurity.`,
+        link: "https://github.com/anthonyhuang07",
         icon: "https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU"
     },
     "3": {
         title: "School",
-        subtitle: "High School Grade 12, IBDP",
-        description: "I am currently a high school student in Grade 12, enrolled in the International Baccalaureate Diploma Programme. I am taking HL English A (Language & Literature), HL French B, HL Geography, SL Chemistry, SL Physics, SL Mathematics AA.\n\nI am the Co-President of the Practical Section of our school's Computer Science Club, one of the biggest clubs at the school.\n\nI am an incoming Computer Science student at Carleton University, where I also hope to do a Linguistics minor. I am currently applying to Shopify Dev Degree.",
-        openLink: "#",
+        subtitle: "High School Grade 12, IBDP.",
+        description: `I am currently a Grade 12 high school student enrolled in the International Baccalaureate Diploma Programme, and I would describe myself as a high-80s student academically. My courses include: 
+
+        • HL English A (Language & Literature)
+        • HL French B
+        • HL Geography
+        • SL Chemistry
+        • SL Physics [Level 5]
+        • SL Mathematics (Analysis & Approaches) [Level 5]
+        
+        I serve as the Co-President of the Practical Section of our school's Computer Science Club, one of the largest clubs at the school.
+        
+        I am an incoming Computer Science student at Carleton University, where I also plan to pursue a minor in Linguistics. I am also applying to Shopify's Dev Degree program.`,
+        link: "#",
         icon: "https://ictevangelist.com/wp-content/uploads/2015/02/google-classroom.png"
     },
     "4": {
         title: "Languages",
         subtitle: "English, French, Mandarin, Japanese",
-        description: "I speak fluent English, B1 French, A2 Mandarin and A1 Japanese.\n\nI was born and lived in Québec for 8 years, where I spoke English and French on a daily basis (I lived in an Anglophone community in Montréal and indulged in mainly English media as a child). I spoke French at school with friends and used it outside the house. At home, I speak Chinese (Mandarin) and English with my family, as my parents are immigrants from China.\n\nWhen I was 8, I moved to Ontario, where I used English on the daily. While I still took French Immersion classes at school, my French skills quickly started to deteriorate over time as I had no opportunity to use them in daily life. My interest in the language has returned in Grade 12, where I am planning to do the DELF B2.\n\nWhen I was 15, I started watching anime again, and as I was travelling to Japan that summer, I decided to learn a bit of Japanese. Currently, I have my JLPT N5 certificate. I am pretty bad at speaking—as I have nobody to talk in Japanese with. The language has been really easy to learn thanks to my knowledge of Chinese. I am planning to take the JLPT N3 on December 2025. I keep up with my Japanese by studying with flashcards daily, watching Japanese Instagram Reels on my feed, as well as listening and singing Japanese music.",
-        openLink: "#",
+        description: `I speak fluent English, B1 French, A2 Mandarin and A1 Japanese.
+        
+        I spent the first 8 years of my life in Québec, where I spoke English and French on a daily basis (I lived in an Anglophone community in Montréal and indulged in mainly English media as a child). I spoke French at school with friends and used it outside the house. At home, I speak Chinese (Mandarin) and English with my family, as my parents are immigrants from China.\n\nWhen I was 8, I moved to Ontario, where I used English on the daily. While I still took French Immersion classes at school, my French skills quickly started to deteriorate over time as I had no opportunity to use them in daily life. My interest in the language has returned in Grade 12, where I am planning to do the DELF B2.\n\nWhen I was 15, I started watching anime again, and as I was travelling to Japan that summer, I decided to learn a bit of Japanese. Currently, I have my JLPT N5 certificate. I am pretty bad at speaking—as I have nobody to talk in Japanese with. The language has been really easy to learn thanks to my knowledge of Chinese. I am planning to take the JLPT N3 on December 2025. I keep up with my Japanese by studying with flashcards daily, watching Japanese Instagram Reels on my feed, as well as listening and singing Japanese music.`,
+        link: "#",
         icon: "https://cdn.iconscout.com/icon/free/png-256/free-translate-icon-download-in-svg-png-gif-file-formats--language-translator-translation-ios-ios14-14-pack-user-interface-icons-2365238.png"
     },
     "5": {
         title: "Games",
         subtitle: "Minecraft, Roblox, Rocket League",
         description: "I play a variety of games. I have been playing Minecraft and Roblox for a while, since elementary school.\n\nThe game I currently play most is probably Rocket League, where my peak is Diamond II in 2v2.\n\nThe game I have the most playtime on is Geometry Dash, with over 1000 hours on Steam, and 63 demons completed. My hardest is Future Funk (Hard Demon) and I had a 72% best on Acu (Extreme Demon).",
-        openLink: "https://steamcommunity.com/profiles/76561199118468554/", 
+        link: "https://steamcommunity.com/profiles/76561199118468554/", 
         icon: "/assets/icons/steam.webp"
     },
     "6": {
         title: "Football",
         subtitle: "CF Montréal, CanMNT",
         description: "My favorite sport is football. I am a fan of CF Montréal, my boyhood club (Toronto FC my #1 opp) and I obviously support the Canadian National Team. I have been to 5 Montréal games since 2016... where I saw them lose all of them 😭 (Including 3 games against TFC 🤬).\n\nI follow what happens in the world of football daily. I don't support any Premier League club, but I guess I can consider myself a Bayern München supporter, though my support for them is nowhere near my support for CF Montréal.\n\nIn real life, I played house league at my local soccer club on and off, and I can play any position on either wing. I can even play as backup goalkeeper because I have very strong reaction time and reflexes.",
-        openLink: "#",
+        link: "#",
         icon: "https://play-lh.googleusercontent.com/6L1u_v2JGG4Rpkh6UuHD8m5xNDMddxUTp6lcO86fgTzJ-SC8Fphgz7CkLZklnS8Dfjs"
     },
     "7": {
         title: "Anime",
         subtitle: "The only TV I watch",
         description: "I would say I watch a decent amount of anime. I first started watching anime in 2019, with my first being Dragon Ball Super (why did I start with Super? I don't know). Then in 2020, I watched JoJo's Bizarre Adventure, which became my favorite and the primary focus of my YouTube channel back then (I was a Roblox JoJo YouTuber). Unfortunately, due to some cyberbullying in that community (I was 12 at the time) I told myself I would never watch anime again.\n\nIn 2023, I was friends with people who were in the anime/manga scene, so I decided to give anime another try, watching Oshi no Ko (which just aired) and later Bocchi the Rock. These animes sparked my interest in learning Japanese.\n\nMy current favorites include Dragon Ball, JoJo's Bizarre Adventure, Pokémon, Steins;Gate, Bocchi the Rock!, and Horimiya.",
-        openLink: "https://myanimelist.net/profile/FHDHGNGN",
+        link: "https://myanimelist.net/profile/FHDHGNGN",
         icon: "https://play-lh.googleusercontent.com/zVwzSU7ozKU0x78V7zYWDw2XFjgGsBBJA_qIJQXAFnS1R3VemFbpdaV9Bm3zOTTHvXw"
     },
     "8": {
         title: "Food",
         subtitle: "Cuisine",
         description: "I appreciate a wide range of foods, from juicy burgers to exquisite Japanese dishes that never fail to excite my taste buds.",
-        openLink: "#",
+        link: "#",
         icon: "https://play-lh.googleusercontent.com/CSOQQrprvW0fn000Vo0P1NC1NWyQh4pKyL7ErjYC5vzJ_sHM8VjxtF6M9VUfRwWCvQM"
     },
     "9": {
         title: "My Setup",
         subtitle: "My Devices",
         description: "I proudly use an M2 MacBook Air paired with a Keychron K8 featuring Milky Yellows, showcasing my blend of style and functionality.",
-        openLink: "#",
+        link: "#",
         icon: "https://cdn.jim-nielsen.com/ios/512/apple-support-2017-06-07.png?rf=1024"
     },
     "10": {
@@ -84,18 +103,13 @@ const appDetails = {
 
                 Nowadays though, the FHD in FHDHGNGN kinda means 'Full HD' as in 1080p.
         `,
-        openLink: "#",
+        link: "#",
         icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_Notes_icon.svg/1024px-Apple_Notes_icon.svg.png"
     }
 };
 
-// Modify populateAppCards to remain unchanged
 function populateAppCards() {
     const appsContainer = document.getElementById("apps");
-    if (!appsContainer) {
-        console.error("#apps container not found!");
-        return;
-    }
     let html = "";
     Object.keys(appDetails).forEach(rank => {
         const details = appDetails[rank];
@@ -153,13 +167,13 @@ function attachViewButtonListeners() {
             document.querySelector(".appView header h2").innerText = details.subtitle;
             document.querySelector(".appStore-appView-about p").innerText = details.description;
             const openButton = document.querySelector(".appStore-openButton");
-            if(!details.openLink.trim() || details.openLink === "#"){
+            if(!details.link.trim() || details.link === "#"){
                 openButton.classList.add("disabled");
                 openButton.onclick = null;
             } else {
                 openButton.classList.remove("disabled");
                 openButton.onclick = () => {
-                    window.open(details.openLink, "_blank");
+                    window.open(details.link, "_blank");
                 };
             }
             document.querySelector(".appView header img").src = details.icon;
