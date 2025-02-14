218f4e7292c6bd48a3720df024f530d9adb20d2e
Anthony Huang <anthonyhuang07@icloud.com>
2025-02-14 00:03:53 -0500

    adjust grid layout in aboutme.css and update app details in aboutme.js

--- src/js/aboutme.js
+++ src/js/aboutme.js
@@ -21,62 +21,48 @@ const appDetails = {
         icon: "https://ictevangelist.com/wp-content/uploads/2015/02/google-classroom.png"
     },
     "4": {
-        title: "Title",
-        subtitle: "Subtitle",
-        description: "I've participated in 5 hackathons, winning awards in 2; Hack the Ridge 2023 (Solo Male Winner) and Hack the Ridge 2024 (Best .xyz Use). In all five hackathons, I made websites using HTML/CSS/JS/TS.\n\nI have also participated in the Canadian Computing Competition, Junior Division, where I scored 56/75 (I TLE'd on J4 💀). I use Python for competitive programming.",
-        openLink: "#",
-        icon: "https://placehold.co/100x100"
-    },
-    "5": {
         title: "Languages",
         subtitle: "English, French, Mandarin, Japanese",
         description: "I speak fluent English, B1 French, A2 Mandarin and A1 Japanese.\n\nI was born and lived in Québec for 8 years, where I spoke English and French on a daily basis (I lived in an Anglophone community in Montréal and indulged in mainly English media as a child). I spoke French at school with friends and used it outside the house. At home, I speak Chinese (Mandarin) and English with my family, as my parents are immigrants from China.\n\nWhen I was 8, I moved to Ontario, where I used English on the daily. While I still took French Immersion classes at school, my French skills quickly started to deteriorate over time as I had no opportunity to use them in daily life. My interest in the language has returned in Grade 12, where I am planning to do the DELF B2.\n\nWhen I was 15, I started watching anime again, and as I was travelling to Japan that summer, I decided to learn a bit of Japanese. Currently, I have my JLPT N5 certificate. I am pretty bad at speaking—as I have nobody to talk in Japanese with. The language has been really easy to learn thanks to my knowledge of Chinese. I am planning to take the JLPT N3 on December 2025. I keep up with my Japanese by studying with flashcards daily, watching Japanese Instagram Reels on my feed, as well as listening and singing Japanese music.",
         openLink: "#",
         icon: "https://cdn.iconscout.com/icon/free/png-256/free-translate-icon-download-in-svg-png-gif-file-formats--language-translator-translation-ios-ios14-14-pack-user-interface-icons-2365238.png"
     },
-    "6": {
+    "5": {
         title: "Games",
         subtitle: "Minecraft, Roblox, Rocket League",
         description: "I play a variety of games. I have been playing Minecraft and Roblox for a while, since elementary school.\n\nThe game I currently play most is probably Rocket League, where my peak is Diamond II in 2v2.\n\nThe game I have the most playtime on is Geometry Dash, with over 1000 hours on Steam, and 63 demons completed. My hardest is Future Funk (Hard Demon) and I had a 72% best on Acu (Extreme Demon).",
         openLink: "https://steamcommunity.com/profiles/76561199118468554/", 
         icon: "/assets/icons/steam.webp"
     },
-    "7": {
+    "6": {
         title: "Football",
         subtitle: "CF Montréal, CanMNT",
         description: "My favorite sport is football. I am a fan of CF Montréal, my boyhood club (Toronto FC my #1 opp) and I obviously support the Canadian National Team. I have been to 5 Montréal games since 2016... where I saw them lose all of them 😭 (Including 3 games against TFC 🤬).\n\nI follow what happens in the world of football daily. I don't support any Premier League club, but I guess I can consider myself a Bayern München supporter, though my support for them is nowhere near my support for CF Montréal.\n\nIn real life, I played house league at my local soccer club on and off, and I can play any position on either wing. I can even play as backup goalkeeper because I have very strong reaction time and reflexes.",
         openLink: "#",
         icon: "https://play-lh.googleusercontent.com/6L1u_v2JGG4Rpkh6UuHD8m5xNDMddxUTp6lcO86fgTzJ-SC8Fphgz7CkLZklnS8Dfjs"
     },
-    "8": {
+    "7": {
         title: "Anime",
         subtitle: "The only TV I watch",
         description: "I would say I watch a decent amount of anime. I first started watching anime in 2019, with my first being Dragon Ball Super (why did I start with Super? I don't know). Then in 2020, I watched JoJo's Bizarre Adventure, which became my favorite and the primary focus of my YouTube channel back then (I was a Roblox JoJo YouTuber). Unfortunately, due to some cyberbullying in that community (I was 12 at the time) I told myself I would never watch anime again.\n\nIn 2023, I was friends with people who were in the anime/manga scene, so I decided to give anime another try, watching Oshi no Ko (which just aired) and later Bocchi the Rock. These animes sparked my interest in learning Japanese.\n\nMy current favorites include Dragon Ball, JoJo's Bizarre Adventure, Pokémon, Steins;Gate, Bocchi the Rock!, and Horimiya.",
         openLink: "https://myanimelist.net/profile/FHDHGNGN",
         icon: "https://play-lh.googleusercontent.com/zVwzSU7ozKU0x78V7zYWDw2XFjgGsBBJA_qIJQXAFnS1R3VemFbpdaV9Bm3zOTTHvXw"
     },
-    "9": {
-        title: "Favorites",
-        subtitle: "Personal Picks",
-        description: "My favorite color is #FF7700 and my lucky number is 4, details that reflect my unique style and personality.",
-        openLink: "#",
-        icon: "https://play-lh.googleusercontent.com/FpCCoNLOt6LRIY_3NM5Rk_LDN-kFNz0yxdFjm-CYM4XavRQfoQlXxOtgC7abfexIDOE"
-    },
-    "10": {
+    "8": {
         title: "Food",
         subtitle: "Cuisine",
         description: "I appreciate a wide range of foods, from juicy burgers to exquisite Japanese dishes that never fail to excite my taste buds.",
         openLink: "#",
         icon: "https://play-lh.googleusercontent.com/CSOQQrprvW0fn000Vo0P1NC1NWyQh4pKyL7ErjYC5vzJ_sHM8VjxtF6M9VUfRwWCvQM"
     },
-    "11": {
+    "9": {
         title: "My Setup",
         subtitle: "My Devices",
         description: "I proudly use an M2 MacBook Air paired with a Keychron K8 featuring Milky Yellows, showcasing my blend of style and functionality.",
         openLink: "#",
         icon: "https://cdn.jim-nielsen.com/ios/512/apple-support-2017-06-07.png?rf=1024"
     },
-    "12": {
+    "10": {
         title: "FHDHGNGN",
         subtitle: "Username Meaning",
         description: `
