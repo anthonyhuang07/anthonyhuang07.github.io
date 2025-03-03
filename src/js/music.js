0771104f28b96f8b92cc7911a60368c23eac2927
Anthony Huang <anthonyhuang07@icloud.com>
2025-03-03 14:39:05 -0500

    dynamic island

--- src/js/music.js
+++ src/js/music.js
@@ -1,8 +1,6 @@
-const nowplaying = document.getElementById('nowplaying');
-const art = document.getElementById('art');
+const dynamicIsland = document.getElementById('dynamicIsland');
+const albumArt = dynamicIsland.querySelector('img');
 const apiUrl = 'https://server.ah07.xyz/api/status';
-
-nowplaying.innerHTML = ` Nothing.`;
 
 function nowPlaying() {
   fetch(apiUrl)
@@ -13,39 +11,23 @@ function nowPlaying() {
       return response.json();
     })
     .then(data => {
-      let timeLeft = '';
+      let found = false;
       for (let i = 0; i < data.status.length; i++) {
         if (data.status[i].name === "Apple Music") {
-          const endTimestamp = new Date(data.status[i].timestamps.end);
-          const currentTime = new Date();
-          let timeDifference = endTimestamp - currentTime;
-
-          timeDifference = Math.max(timeDifference, 0);
-
-          // Convert time difference to seconds
-          const totalSeconds = Math.floor(timeDifference / 1000);
-          const minutes = Math.floor(totalSeconds / 60);
-          const seconds = totalSeconds % 60;
-
-          timeLeft = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-
-          nowplaying.innerHTML = `<b>${data.status[i].details}</b><br>${data.status[i].state}<br>${timeLeft} left`;
-          art.src = `https://${(data.status[i].assets.largeImage).replace(/^.*?is1/, 'is1')}`
-          art.title = `${data.status[i].assets.largeText}`
+          let imageUrl = data.status[i].assets.largeImage;
+          if (!imageUrl.startsWith('https://')) {
+            imageUrl = 'https://' + imageUrl.replace(/^.*?is1/, 'is1');
+          }
+          albumArt.src = imageUrl;
+          found = true;
           break;
-        } else {
-          nowplaying.innerHTML = ` Nothing.`;
-          art.src = ``
-          art.title = ``
         }
       }
     })
     .catch(error => {
       console.error('Error fetching data:', error);
     });
-
 }
 
-nowPlaying()
-
+nowPlaying();
 setInterval(nowPlaying, 1000);
