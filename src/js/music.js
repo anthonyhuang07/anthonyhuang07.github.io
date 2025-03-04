50c98b9235cba73f12365cf52123b21c574c234c
Anthony Huang <anthonyhuang07@icloud.com>
2025-03-03 21:58:01 -0500

    refactor: update dynamic island styles and improve image handling

--- src/js/music.js
+++ src/js/music.js
@@ -2,26 +2,31 @@ const dynamicIsland = document.getElementById('dynamicIsland');
 const albumArt = dynamicIsland.querySelector('img');
 const apiUrl = 'https://server.ah07.xyz/api/status';
 
+// Create a hidden canvas element
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+
 function nowPlaying() {
   fetch(apiUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
       for (let i = 0; i < data.status.length; i++) {
         if (data.status[i].name === "Apple Music") {
           let imageUrl = data.status[i].assets.largeImage;
 
+          // Fix mp:external URLs
           if (imageUrl.startsWith('mp:external/')) {
             let fixedUrl = imageUrl.split('/https/').pop();
             imageUrl = 'https://' + fixedUrl;
           }
 
-          // Apply the (potentially fixed) URL
+          albumArt.crossOrigin = "Anonymous";
           albumArt.src = imageUrl;
+
+          albumArt.onload = () => {
+            applyGradientEffect(albumArt);
+          };
+
           break;
         }
       }
