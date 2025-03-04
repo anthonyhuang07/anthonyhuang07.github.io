735fcdaee32f266efef818fca3763bc6c6dc3bb7
Anthony Huang <anthonyhuang07@icloud.com>
2025-03-03 22:16:41 -0500

    Lore Accurate dynamic island?

--- src/js/music.js
+++ src/js/music.js
@@ -1,8 +1,8 @@
 const dynamicIsland = document.getElementById('dynamicIsland');
 const albumArt = dynamicIsland.querySelector('img');
+const audioPreview = dynamicIsland.querySelector('#audioPreview');
 const apiUrl = 'https://server.ah07.xyz/api/status';
 
-// Create a hidden canvas element
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -10,11 +10,12 @@ function nowPlaying() {
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
+      let musicPlaying = false;
       for (let i = 0; i < data.status.length; i++) {
         if (data.status[i].name === "Apple Music") {
+          musicPlaying = true;
           let imageUrl = data.status[i].assets.largeImage;
 
-          // Fix mp:external URLs
           if (imageUrl.startsWith('mp:external/')) {
             let fixedUrl = imageUrl.split('/https/').pop();
             imageUrl = 'https://' + fixedUrl;
@@ -30,10 +31,56 @@ function nowPlaying() {
           break;
         }
       }
+
+      if (!musicPlaying) {
+        albumArt.style.display = 'none';
+        audioPreview.style.display = 'none';
+      } else {
+        albumArt.style.display = 'block';
+        audioPreview.style.display = 'flex';
+      }
     })
     .catch(error => {
       console.error('Error fetching data:', error);
     });
+}
+
+function applyGradientEffect(imgElement) {
+  const bars = document.querySelectorAll('.bar');
+
+  canvas.width = imgElement.naturalWidth;
+  canvas.height = imgElement.naturalHeight;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.drawImage(imgElement, 0, 0, canvas.width, canvas.height);
+
+  ctx.globalAlpha = 1;
+  ctx.filter = 'blur(15px)';
+  ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+  ctx.filter = 'none';
+
+  bars.forEach((bar, index) => {
+    const barCanvas = document.createElement('canvas');
+    const barCtx = barCanvas.getContext('2d');
+    barCanvas.width = canvas.width / bars.length * 1.2;
+    barCanvas.height = canvas.height * 0.6;
+
+    barCtx.drawImage(
+      canvas,
+      index * barCanvas.width * 0.8,
+      canvas.height * 0.2,
+      barCanvas.width,
+      barCanvas.height,
+      0,
+      0,
+      barCanvas.width,
+      barCanvas.height
+    );
+
+    bar.style.backgroundImage = `url(${barCanvas.toDataURL()})`;
+    bar.style.backgroundSize = 'cover';
+    bar.style.backgroundPosition = 'center';
+  });
 }
 
 nowPlaying();
