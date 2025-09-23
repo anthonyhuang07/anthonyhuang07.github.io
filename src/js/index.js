0b89028b2ee06fc62640338e65f497cdbf4ba40b
Anthony Huang <anthonyhuang07@icloud.com>
2025-09-23 12:34:11 -0400

    aboutme changes

--- src/js/index.js
+++ src/js/index.js
@@ -76,7 +76,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const backgrounds = [
     "assets/backgrounds/0.webp",
     "assets/backgrounds/1.webp",
-    "assets/backgrounds/2.webp",
+    "assets/backgrounds/2.jpg",
+    "assets/backgrounds/3.jpg",
   ];
   const randomBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
   const bgDiv = document.getElementById("bg");
