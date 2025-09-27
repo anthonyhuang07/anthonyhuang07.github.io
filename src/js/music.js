d3e0f87dba9d930b6259b16be1217eab7cc5a488
Anthony Huang <anthonyhuang07@icloud.com>
2025-09-27 15:51:10 -0400

    FEATURED ALBUMS ALRAEDY WHAAAAT

--- src/js/music.js
+++ src/js/music.js
@@ -14,70 +14,7 @@ const ctx = canvas.getContext('2d');
 
 let intervalId;
 
-const favoriteSongDetails = {
-  favoriteDate: "SEP 24, 2025",
-  songName: "DEADPOOL (feat. KAF)",
-  artistName: "Hoshimachi Suisei",
-  albumArt: "https://images.genius.com/eeb2490ea2a00ea03ccec34e073e8cbe.1000x1000x1.png",
-  link: "https://www.youtube.com/watch?v=Fg5-55vZwp0"
-};
-
-const featuredSongs = [
-  {
-    title: "Show",
-    artist: "Ado",
-    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/80/e8/82/80e8826c-f877-9a74-0e7b-a142108feda0/23UMGIM92958.rgb.jpg/600x600bf-60.jpg",
-    link: "https://www.youtube.com/watch?v=pgXpM4l_MwI"
-  },
-  {
-    title: "AWAKE",
-    artist: "Hoshimachi Suisei",
-    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/af/02/ed/af02ede0-cf69-ed76-65fd-7f969a30a409/HoshimachiSuisei_AWAKE_JKT.png/600x600bf-60.jpg",
-    link: "https://www.youtube.com/watch?v=K9YGGOd63yc"
-  },
-  {
-    title: "Aimoraimo",
-    artist: "tuki.",
-    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/29/0f/2c/290f2ce9-6af3-4ace-e5f7-d9c9df751c0e/4582649322263.jpg/600x600bf-60.jpg",
-    link: "https://www.youtube.com/watch?v=urh7DZp59WU"
-  },
-  {
-    title: "Moral Crumble",
-    artist: "FantasticYouth",
-    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/2f/87/a3/2f87a3ac-624e-70e6-76a6-1ec1a409e706/21UMGIM25282.rgb.jpg/1200x630bb.jpg",
-    link: "https://www.youtube.com/watch?v=Z3dVAI0_YTI"
-  },
-  {
-    title: "Monotone",
-    artist: "YOASOBI",
-    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/df/05/a9/df05a94d-ff41-e0e1-f156-f40ee7c9922b/198846242873.jpg/486x486bb.png",
-    link: "https://www.youtube.com/watch?v=sJ-2X3rHtXw"
-  },
-  {
-    title: "Circle",
-    artist: "Lilas Ikuta",
-    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/d4/3e/cb/d43ecbf0-43bd-6507-3faa-a5a29f1683be/197188156664.jpg/1200x630bb.jpg",
-    link: "https://example.com/track-5"
-  },
-    {
-    title: "Huji",
-    artist: "Trooper Salute",
-    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/96/43/2a/96432a2b-764a-874a-4e82-5793f1c8dace/4580789718663.png/1200x630bb.jpg",
-    link: "https://www.youtube.com/watch?v=_ti7ovjaDG4"
-  },
-  {
-    title: "MURI MURI EVOLUTION",
-    artist: "NANAOAKARI",
-    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/9d/a9/11/9da911e3-c3ee-7f2c-ca38-3fc34850885f/4547366756616.jpg/1200x630bb.jpg",
-    link: "https://www.youtube.com/watch?v=o4OsvOqHnZM"
-  },
-  {
-    title: "Tokihanate!",
-    artist: "Hashimero",
-    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/25/ec/de/25ecde3a-0028-c1d1-4b66-5c3809d2cd52/4547366757477.jpg/1200x630bb.jpg",
-    link: "https://www.youtube.com/watch?v=yDChK7w4II8"
-  },
-];
+// #region Dynamic Island & Now Playing
 
 function nowPlaying() {
   fetch(apiUrl)
@@ -153,7 +90,7 @@ function nowPlaying() {
 
         nowPlayingArt.src = '/assets/icons/defaultMusic.webp';
         nowPlayingTextContainer.innerHTML = `
-          <p>Not Playing</p>
+          <p>I'm not listening to anything right now!</p>
         `;
       } else {
         dynamicIsland.classList.add('playing');
@@ -213,6 +150,18 @@ function applyGradientEffect(imgElement) {
   });
 }
 
+// #endregion
+
+// #region Favorite Song
+
+const favoriteSongDetails = {
+  favoriteDate: "SEP 24, 2025",
+  songName: "DEADPOOL (feat. KAF)",
+  artistName: "Hoshimachi Suisei",
+  albumArt: "https://images.genius.com/eeb2490ea2a00ea03ccec34e073e8cbe.1000x1000x1.png",
+  link: "https://www.youtube.com/watch?v=Fg5-55vZwp0"
+};
+
 function renderFavoriteSong() {
   const favoriteSongContainer = document.querySelector('.music-favoriteSong');
   favoriteSongContainer.innerHTML = `
@@ -230,6 +179,67 @@ function renderFavoriteSong() {
     </div>
   `;
 }
+
+// #endregion
+
+// #region Featured Songs
+
+const featuredSongs = [
+  {
+    title: "Show",
+    artist: "Ado",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/80/e8/82/80e8826c-f877-9a74-0e7b-a142108feda0/23UMGIM92958.rgb.jpg/600x600bf-60.jpg",
+    link: "https://www.youtube.com/watch?v=pgXpM4l_MwI"
+  },
+  {
+    title: "AWAKE",
+    artist: "Hoshimachi Suisei",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/af/02/ed/af02ede0-cf69-ed76-65fd-7f969a30a409/HoshimachiSuisei_AWAKE_JKT.png/600x600bf-60.jpg",
+    link: "https://www.youtube.com/watch?v=K9YGGOd63yc"
+  },
+  {
+    title: "Aimoraimo",
+    artist: "tuki.",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/29/0f/2c/290f2ce9-6af3-4ace-e5f7-d9c9df751c0e/4582649322263.jpg/600x600bf-60.jpg",
+    link: "https://www.youtube.com/watch?v=urh7DZp59WU"
+  },
+  {
+    title: "Moral Crumble",
+    artist: "FantasticYouth",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/2f/87/a3/2f87a3ac-624e-70e6-76a6-1ec1a409e706/21UMGIM25282.rgb.jpg/1200x630bb.jpg",
+    link: "https://www.youtube.com/watch?v=Z3dVAI0_YTI"
+  },
+  {
+    title: "Monotone",
+    artist: "YOASOBI",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/df/05/a9/df05a94d-ff41-e0e1-f156-f40ee7c9922b/198846242873.jpg/486x486bb.png",
+    link: "https://www.youtube.com/watch?v=sJ-2X3rHtXw"
+  },
+  {
+    title: "Circle",
+    artist: "Lilas Ikuta",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/d4/3e/cb/d43ecbf0-43bd-6507-3faa-a5a29f1683be/197188156664.jpg/1200x630bb.jpg",
+    link: "https://example.com/track-5"
+  },
+  {
+    title: "Huji",
+    artist: "Trooper Salute",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/96/43/2a/96432a2b-764a-874a-4e82-5793f1c8dace/4580789718663.png/1200x630bb.jpg",
+    link: "https://www.youtube.com/watch?v=_ti7ovjaDG4"
+  },
+  {
+    title: "MURI MURI EVOLUTION",
+    artist: "NANAOAKARI",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/9d/a9/11/9da911e3-c3ee-7f2c-ca38-3fc34850885f/4547366756616.jpg/1200x630bb.jpg",
+    link: "https://www.youtube.com/watch?v=o4OsvOqHnZM"
+  },
+  {
+    title: "Tokihanate!",
+    artist: "Hashimero",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/25/ec/de/25ecde3a-0028-c1d1-4b66-5c3809d2cd52/4547366757477.jpg/1200x630bb.jpg",
+    link: "https://www.youtube.com/watch?v=yDChK7w4II8"
+  },
+];
 
 function renderfeaturedSongs() {
   const container = document.querySelector('.music-featuredSongs');
@@ -257,9 +267,76 @@ function renderfeaturedSongs() {
   `;
 }
 
+// #endregion
+
+// #region Featured Albums
+
+const featuredAlbums = [
+  {
+    title: "Ado's Utattemita Album",
+    artist: "Ado",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/fa/e9/42/fae942a7-dca5-6cc0-9e49-4a4f2da6b81e/23UM1IM45777.rgb.jpg/600x600bf-60.jpg",
+    link: "https://music.apple.com/album/ados-utattemita-album/1718531990"
+  },
+  {
+    title: "SHINSEI MOKUROKU",
+    artist: "Hoshimachi Suisei",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/33/8a/cd/338acd51-fd74-ca01-474c-128f2575b753/4571696527127.jpg/600x600bf-60.jpg",
+    link: "https://music.apple.com/album/shinsei-mokuroku/1789078173"
+  },
+  {
+    title: "15",
+    artist: "tuki.",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/f3/c0/47/f3c04781-71d4-1a2c-5a50-81e68bddcc5d/4582649401234.jpg/600x600bf-60.jpg",
+    link: "https://music.apple.com/album/15/1781833057"
+  },
+  {
+    title: "BlueGuns",
+    artist: "FantasicYouth",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/2f/87/a3/2f87a3ac-624e-70e6-76a6-1ec1a409e706/21UMGIM25282.rgb.jpg/486x486bb.png",
+    link: "https://music.apple.com/album/blueguns/1562602234"
+  },
+  {
+    title: "Kessoku Band",
+    artist: "kessoku band",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music122/v4/51/3b/00/513b003d-5583-4533-168b-f213c245b53f/4534530141828.jpg/600x600bf-60.jpg",
+    link: "https://music.apple.com/album/%E7%B5%90%E6%9D%9F%E3%83%90%E3%83%B3%E3%83%89/1657318546"
+  },
+  {
+    title: "Sketch",
+    artist: "Lilas Ikuta",
+    albumArt: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/d4/3e/cb/d43ecbf0-43bd-6507-3faa-a5a29f1683be/197188156664.jpg/600x600bf-60.jpg",
+    link: "https://music.apple.com/album/sketch/1671948398"
+  },
+];
+
+function renderFeaturedAlbums() {
+  const container = document.querySelector('.music-featuredAlbums');
+  if (!container) return;
+
+  const items = featuredAlbums;
+  const gridHtml = items
+    .map(album => `
+      <a class="album" href="${album.link}" target="_blank" rel="noopener noreferrer" aria-label="Open album ${album.title} by ${album.artist}">
+        <img src="${album.albumArt}" alt="Album art for ${album.title} by ${album.artist}" />
+        <p class="album-title">${album.title}</p>
+        <p class="album-artist">${album.artist}</p>
+      </a>
+    `)
+    .join('');
+
+  container.innerHTML = `
+    <h2>Featured Albums</h2>
+    <div class="albums-row">${gridHtml}</div>
+  `;
+}
+
+// #endregion
+
 document.addEventListener("DOMContentLoaded", () => {
   renderFavoriteSong();
   renderfeaturedSongs();
+  renderFeaturedAlbums();
   nowPlaying();
   setInterval(nowPlaying, 1000);
 });
