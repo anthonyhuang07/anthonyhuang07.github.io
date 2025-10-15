296ae3be656629f612c98a01ef7c72b339c7eb1e
Anthony Huang <anthonyhuang07@icloud.com>
2025-10-15 01:45:39 -0400

    projects. selfstats added

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,26 +1,31 @@
 const projectDetails = {
   "Ontario IB Mark Converter": {
-    description: "Converts your Raw IB Mark to a Converted OSSD Mark.",
+    description: "Converts your Raw IB Mark to a Converted OSSD Mark. (Used by ~300 IBDP students, 8000+ Google clicks)",
     link: "http://ibconverter.com/",
     icon: "https://www.ibschoolsofontario.ca/wp-content/uploads/2018/12/IB-world-shools-1.png"
   },
   "PomodoPro": {
-    description: "A minimalistic and customizable Pomodoro experience.",
+    description: "A minimalistic and customizable Pomodoro experience. Began as a solo Hackathon project.",
     link: "https://pomodopro.app/",
     icon: "https://github.com/anthonyhuang07/PomodoPro/blob/main/assets/pomodoProLogo.png?raw=true"
   },
-  "Piracy": {
-    description: "Piracy is a serious crime. Click for more info...",
-    link: "https://anthonyhuang.net/piracy/",
-    icon: "https://static.vecteezy.com/system/resources/thumbnails/012/042/301/small_2x/warning-sign-icon-transparent-background-free-png.png"
-  },
   "ByteBucks": {
-    description: "A basic Adventure Capitalist-inspired game made for my high school CS course.",
+    description: "An Adventure Capitalist-inspired game made for my Grade 11 High School CS course (100% Final).",
     link: "https://anthonyhuang.net/ICS3U1-FPT/",
     icon: "/assets/icons/bytebucks.png"
   },
+  "SelfStats": {
+    description: "A hub for simple, everyday health tools. Solo Hackathon-Winning project which took under 12 hours to build.",
+    link: "https://anthonyhuang.net/SelfStats/",
+    icon: "/assets/icons/selfstats.png"
+  },
+  "Piracy": {
+    description: "Piracy is a serious crime. Click for more info... (Prank Website)",
+    link: "https://anthonyhuang.net/piracy/",
+    icon: "https://static.vecteezy.com/system/resources/thumbnails/012/042/301/small_2x/warning-sign-icon-transparent-background-free-png.png"
+  },
   "Metropolitan French VS Québec French": {
-    description: "My IB Extended Essay (~4000 word academic paper) about Parisian French VS Québec French.",
+    description: "My IB Extended Essay (~4000 word academic paper) comparing Parisian and Québec French linguistically.",
     link: "https://docs.google.com/document/d/1_EMppz874MndkfzhzCydNiehpn5TGKabTHW_8_mD1lI/edit?usp=sharing",
     icon: "/assets/icons/quebec.webp"
   },
