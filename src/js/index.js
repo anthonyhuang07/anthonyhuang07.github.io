92b652fa1125df259cbfc5b7bbb2dfd54ff4098c
Anthony Huang <anthonyhuang07@icloud.com>
2026-01-11 17:09:00 -0500

    font awesome faster loading

--- src/js/index.js
+++ src/js/index.js
@@ -82,7 +82,6 @@ document.addEventListener("DOMContentLoaded", () => {
     "/assets/backgrounds/0.webp",
     "/assets/backgrounds/1.webp",
     "/assets/backgrounds/2.webp",
-    // "assets/backgrounds/4.jpg",
   ];
   const randomBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
   const bgDiv = document.getElementById("bg");
